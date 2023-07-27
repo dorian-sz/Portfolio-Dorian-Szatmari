b6106d694ff82a991514793c92a54881254033ec
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-07-27 10:40:32 +0200

    Add react router dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './variables.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Routes>
+      <Route></Route>
+    </Routes>
   );
 }
 
