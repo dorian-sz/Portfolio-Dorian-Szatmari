102bebf15ce12b1e284db84f20b22bae92f1f467
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-07-29 09:57:08 +0200

    Add Navbar component

---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+      <nav className='nav-container'>
+          <div className="nav">
+              <ul className='nav-list'>
+                <div className="nav-li-container">
+                  <li className='nav-li'>
+                    <Link to="/" className='nav-link'>Home</Link>
+                  </li>
+                </div>        
+                <div className="nav-li-container">
+                  <li className='nav-li'>
+                    <Link to="/about-me" className='nav-link'>About me</Link>
+                  </li>
+                </div>        
+                <div className="nav-li-container">
+                  <li className='nav-li'>
+                    <Link to="/projects" className='nav-link'>Projects</Link>
+                  </li>
+                </div>    
+                <div className="nav-li-container">
+                  <li className='nav-li'>
+                  <Link to="/curriculum-vitae" className='nav-link'>Curriculum vitae</Link>
+                  </li>
+                </div> 
+              </ul>
+          </div>
+      </nav>
+  )
+}
+
+export default Navbar
