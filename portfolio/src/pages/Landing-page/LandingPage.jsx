9f82a0fef5fcc07147a6fe63d57a25b3645ee18c
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-07-29 21:18:35 +0200

    Add LandingPage animated greeting

---
+++ portfolio/src/pages/Landing-page/LandingPage.jsx
@@ -0,0 +1,20 @@
+import "./LandingPage.css";
+import feeling_proud from "../../img/feeling_proud.svg"
+const LandingPage = () => {
+  return (
+        <div className='landing-page-container'>
+            <div className="landing-page-introduction-container">
+                <div className="landing-page-greeting-container">
+                    <p className="landing-page-greeting">I'm</p>
+                    <div className="landing-page-greeting-animation">
+                        <div className="landing-page-greeting-first"><div>&#160;Szatmári Dorián</div></div>
+                        <div className="landing-page-greeting-second">&#160;a <div>Full-stack Developer</div></div>
+                        <div className="landing-page-greeting-third">&#160;a <div>Codecool Student</div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default LandingPage
