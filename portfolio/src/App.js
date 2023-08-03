72436f2a99734bbe56094d507d64fdcc3774766f
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-08-03 23:22:36 +0200

    Add ProjectDetails to App

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ProjectDetails from './components/Projects/ProjectDetails';
 import LandingPage from './pages/Landing-page/LandingPage';
+import Projects from './pages/Projects/Projects';
 import './variables.css';
 import {Route, Routes} from "react-router-dom";
 
 function App() {
+  const [displayDetails, setDisplayDetails] = useState(false);
+
+  const setDisplay = () => {
+    setDisplayDetails(!displayDetails);
+  }
+
   return (
-    <div className='App'>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}></Route>
-      </Routes>
+    <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}></Route>
+          <Route path='/projects' element={<Projects setDisplayDetails={setDisplay}/>}></Route>
+        </Routes>
+        {displayDetails && <ProjectDetails setDisplay={setDisplayDetails}/>}
     </div>
   );
 }
