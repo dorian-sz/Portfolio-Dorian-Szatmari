42d8f401455cc974e4a53dee8080b2af269561e3
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-08-03 23:25:06 +0200

    Add font size prop to Tech

--- portfolio/src/components/Projects/Tech.jsx
+++ portfolio/src/components/Projects/Tech.jsx
@@ -1,8 +1,8 @@
 import "./Tech.css"
 
-const Tech = ({techName}) => {
+const Tech = ({techName, fontSize}) => {
   return (
-    <div className='tech-container'><p>{techName}</p></div>
+    <div className='tech-container' style={{fontSize: fontSize}}><p>{techName}</p></div>
   )
 }
 
