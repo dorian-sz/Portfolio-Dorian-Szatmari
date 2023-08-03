bf1506b97bd1dd9eb0eb460f02b43330b299b53d
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-08-03 19:36:23 +0200

    Add ProjectCard

--- portfolio/src/components/Projects/ProjectCard.jsx
+++ portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./ProjectCard.css"
+import Tech from './Tech'
 
-const ProjectCard = () => {
+
+const ProjectCard = ({gif}) => {
+
   return (
-    <div>ProjectCard</div>
+    <div className='project-card-container' style={{backgroundImage: `url("images/mental-health.gif")`}}>
+        <div className="project-card-hover-info">
+            <p className='project-card-title'>Mental Health Journal</p>
+            <p className='project-card-info'>An app that helps you track your mood and tasks for the day.</p>
+            <div className="project-card-tech-container">
+                <Tech techName={"React"}></Tech>
+                <Tech techName={"ASP.NET Core"}></Tech>
+                <Tech techName={"Mssql"}></Tech>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
