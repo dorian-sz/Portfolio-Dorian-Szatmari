f971c3137387c5ac71d52011aadb1a3de4459309
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-08-04 19:55:09 +0200

    Add render from project object

--- portfolio/src/components/Projects/ProjectCard.jsx
+++ portfolio/src/components/Projects/ProjectCard.jsx
@@ -2,21 +2,26 @@ import "./ProjectCard.css"
 import Tech from './Tech'
 
 
-const ProjectCard = ({gif, setDisplayDetails}) => {
-    
+const ProjectCard = ({project, setDisplayDetails, setProject}) => {
+
+  const setupProjectDetails = () =>{
+    setDisplayDetails()
+    setProject(project);
+  }
 
   return (
-    <div className='project-card-container' style={{backgroundImage: `url("images/mental-health.gif")`}} onClick={() => setDisplayDetails()}>
+    <div className='project-card-container' style={{backgroundImage: `url(${project.gif})`}} onClick={() => setupProjectDetails()}>
         <div className="project-card-hover-info">
-            <p className='project-card-title'>Mental Health Journal</p>
-            <p className='project-card-info'>An app that helps you track your mood and tasks for the day.</p>
+            <p className='project-card-title'>{project.projectName}</p>
+            <p className='project-card-info'>{project.shortDescription}</p>
             <div className="project-card-tech-container">
-                <Tech techName={"React"}></Tech>
-                <Tech techName={"Javascript"}></Tech>
-                <Tech techName={"CSS"}></Tech>
-                <Tech techName={"ASP.NET Core"}></Tech>
-                <Tech techName={"Entity Frameworks"}></Tech>
-                <Tech techName={"Mssql"}></Tech>
+              {
+                project.techUsed.map((tech)=>{
+                  return(
+                    <Tech techName={tech}></Tech>
+                  )
+                })
+              }
             </div>
         </div>
     </div>
